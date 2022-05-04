75cc3c60683b4becc5be3517de4597bbfcd83f27
Huijun-Hu <89577056+Huijun-Hu@users.noreply.github.com>
2022-05-03 17:27:36 -0700

    change graphic configuration

--- hangman.asm
+++ hangman.asm
@@ -8,8 +8,8 @@
 # Bitmap Display Configuration:
 # - Unit width in pixels: 4					     
 # - Unit height in pixels: 4
-# - Display width in pixels: 512
-# - Display height in pixels: 256
+# - Display width in pixels: 256
+# - Display height in pixels: 512
 # - Base Address for Display: 0x10010000
 
 .data
@@ -55,8 +55,8 @@ main:
 	######################### Initial Bitmap ###########################################
 	# fill background color
 	la 	$t0, frameBuffer		# load frameBuffer addr
-   	li 	$t1, 0x2000			# save 512x256 pixels
-   	li 	$t2, 0x004A613D			# load color 
+   	li 	$t1, 0x8000			# save 256x512 pixels
+   	li 	$t2, 0x00A9C0AA			# load color 
    bg:
    	sw 	$t2, 0($t0)
    	addi   	$t0, $t0, 4			# advance to next pixel position
@@ -65,44 +65,58 @@ main:
 
 	# draw gallow
 	la 	$t0, frameBuffer
-	addi	$t0, $t0, 5492			# locate left top coner of gallow
-	li	$t1, 20				# gallow horizontal wood width
-	li	$t2, 0x00AC672E			# load gallow color
+	addi	$t0, $t0, 3908			# locate left top coner of gallow
+	li	$t1, 35				# gallow horizontal wood width
+	li	$t2, 0x00663300			# load gallow color
 	
    gallow_top:
    	sw 	$t2, 0($t0)
+   	sw 	$t2, 256($t0)
    	addi   	$t0, $t0, 4			# advance to next pixel position
    	sub 	$t1, $t1, 1			# decrement number of pixel
    	bnez 	$t1, gallow_top
    
 	la 	$t0, frameBuffer
-	addi	$t0, $t0, 6004			# locate 1 pixel down left top coner of gallow
-	li	$t1, 5 				# string  length
+	addi	$t0, $t0, 4208			# locate 1 pixel down left top coner of gallow
+	li	$t1, 8 				# string  length
    gallow_string:
    	sw 	$t2, 0($t0)
-   	addi   	$t0, $t0, 512			# advance to next pixel position
+   	addi   	$t0, $t0, 256			# advance to next pixel position
    	sub 	$t1, $t1, 1			# decrement number of pixel
    	bnez 	$t1, gallow_string
 	
 	la 	$t0, frameBuffer
-	addi	$t0, $t0, 6080			# locate 1 pixel down right top coner of gallow
-	li	$t1, 40 			# stand wood length
+	addi	$t0, $t0, 4552			# locate 1 pixel down right top coner of gallow
+	li	$t1, 92 			# stand wood length
    gallow_stand:
    	sw 	$t2, 0($t0)
-   	addi   	$t0, $t0, 512			# advance to next pixel position
+   	sw 	$t2, 4($t0)
+   	addi   	$t0, $t0, 256			# advance to next pixel position
    	sub 	$t1, $t1, 1			# decrement number of pixel
    	bnez 	$t1, gallow_stand
 
-	sub 	$t0, $t0, 100			# locate start point of gallow base
-	li	$t1, 30 			# base  length
-   gallow_base:
+	sub 	$t0, $t0, 140			# locate start point of gallow base
+	li	$t1, 37 			# base  length
+   gallow_base1:
    	sw 	$t2, 0($t0)
-   	sw 	$t2, 512($t0)
-   	sw 	$t2, 512($t0)
+   	sw 	$t2, 256($t0)
+   	sw 	$t2, 256($t0)
    	addi   	$t0, $t0, 4			# advance to next pixel position
    	sub 	$t1, $t1, 1			# decrement number of pixel
-   	bnez 	$t1, gallow_base
+   	bnez 	$t1, gallow_base1
+   	
+   	add 	$t0, $t0, 516			# locate start point of gallow base
+	li	$t1, 41 			# base  length
+   gallow_base2:
+   	sw 	$t2, 0($t0)
+   	sw 	$t2, 256($t0)
+   	sw 	$t2, 256($t0)
+   	subi   	$t0, $t0, 4			# back to privious pixel position
+   	sub 	$t1, $t1, 1			# decrement number of pixel
+   	bnez 	$t1, gallow_base2
+   	
 ################################ End Initial Bitmap #######################################
+
 
 #display some sort of text and end screen like "Game Over!"	
 game_over:
